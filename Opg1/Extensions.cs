e4f8230e6ab3f18a16edf0bb55edf15358cfdac9
esditm <esd@it-minds.dk>
2014-11-20 10:46:58 +0100

    Initial

---
+++ Opg1/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Opg1
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// Searches for a given value.
+        /// </summary>
+        /// <typeparam name="THaystack">Type of the haystack.</typeparam>
+        /// <typeparam name="TNeedle">Type of the needle.</typeparam>
+        /// <param name="haystack">Haystack to look through.</param>
+        /// <param name="needle">Element to search for.</param>
+        /// <returns>The found elements, if not found returns default value.</returns>
+        public static TNeedle Find<THaystack,TNeedle>(this IEnumerable<THaystack> haystack, TNeedle needle)
+        {
+            // Your implementation goes here!
+            // GL HF
+            return default(TNeedle);
+        }
+    }
+}
---
+++ Opg1/Tests.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Opg1
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Find_SearchFlatStructure_Found()
+        {
+            // Arrange
+            const string needle = "Orange";
+            var haystack = new[] {"Banna", needle, "Apple"};
+            
+            // Act
+            var found = haystack.Find(needle);
+
+            // Assert
+            Assert.AreEqual(needle, found);
+        }
+
+        [Test]
+        public void Find_SearchFlatStructureWithoutNeedle_NotFound()
+        {
+            // Arrange
+            const string needle = "Orange";
+            var haystack = new[] { "Banna", "Apple" };
+            
+            // Act
+            var found = haystack.Find(needle);
+
+            // Assert
+            Assert.IsNull(found);
+        }
+
+        [Test]
+        public void Find_SearchTreeStructure_FoundElement()
+        {
+            // Arrange
+            const string needle = "Ford";
+            var fruits = new List<string> {"Banna", "Orange", "Apple"};
+            var cars = new List<string> {"SAAB", "VW", "Audi", "Volve", needle};
+            var haystack = new List<List<string>> {fruits, cars};
+            
+            // Act
+            var found = haystack.Find(needle);
+
+            // Assert
+            Assert.AreEqual(needle, found);
+        }
+    }
+}
