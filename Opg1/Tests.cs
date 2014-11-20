6d55cae4689a1904f6c0b203cf7e4efd5cb703f2
esditm <esd@it-minds.dk>
2014-11-20 13:02:59 +0100

    Added more tests

--- Opg1/Tests.cs
+++ Opg1/Tests.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 
 namespace Opg1
@@ -7,7 +9,7 @@ namespace Opg1
     public class Tests
     {
         [Test]
-        public void Find_SearchFlatStructure_Found()
+        public void Find_TextSearchFlatStructure_Found()
         {
             // Arrange
             const string needle = "Orange";
@@ -21,12 +23,12 @@ namespace Opg1
         }
 
         [Test]
-        public void Find_SearchFlatStructureWithoutNeedle_NotFound()
+        public void Find_TextSearchFlatStructureWithoutNeedle_NotFound()
         {
             // Arrange
             const string needle = "Orange";
             var haystack = new[] { "Banna", "Apple" };
-            
+
             // Act
             var found = haystack.Find(needle);
 
@@ -35,7 +37,21 @@ namespace Opg1
         }
 
         [Test]
-        public void Find_SearchTreeStructure_FoundElement()
+        public void Find_ObjectSearchFlatStructure_Found()
+        {
+            // Arrange
+            var needle = new Tuple<int>(13);
+            var haystack = new[] { new Tuple<int>(7), new Tuple<int>(9), needle };
+
+            // Act
+            var found = haystack.Find(needle);
+
+            // Assert
+            Assert.AreEqual(needle, found);
+        }
+        
+        [Test]
+        public void Find_TextSearchTreeStructure_Found()
         {
             // Arrange
             const string needle = "Ford";
@@ -48,6 +64,38 @@ namespace Opg1
 
             // Assert
             Assert.AreEqual(needle, found);
+        }
+
+        [Test]
+        public void Find_ObjectSearchTreeStructure_Found()
+        {
+            // Arrange
+            var needle = new Tuple<int>(13);
+            var a = new List<Tuple<int>> {new Tuple<int>(7), new Tuple<int>(9), needle};
+            var b = new List<Tuple<int>> {new Tuple<int>(1), new Tuple<int>(2)};
+            var haystack = new List<List<Tuple<int>>> {a, b};
+
+            // Act
+            var found = haystack.Find(needle);
+
+            // Assert
+            Assert.AreEqual(needle, found);
+        }
+
+        [Test]
+        public void Find_TextSearchTreeStructure_FoundMultiple()
+        {
+            // Arrange
+            const string needle = "Ford";
+            var fruits = new List<string> { "Banna", needle, "Orange", "Apple" };
+            var cars = new List<string> { "SAAB", "VW", "Audi", "Volve", needle };
+            var haystack = new List<List<string>> { fruits, cars };
+
+            // Act
+            var found = haystack.Find(needle);
+
+            // Assert
+            Assert.That(found.Count() == 2);
         }
     }
 }
